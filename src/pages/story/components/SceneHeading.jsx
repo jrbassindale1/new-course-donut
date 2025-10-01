63e87e0f8535b66c2f02344c9bb3ec23ae5a8bfc
Jonathan Bassindale <jonathan.bassindale@uwe.ac.uk>
2025-10-01 10:00:52 +0100

    small chnages

--- src/pages/story/components/SceneHeading.jsx
+++ src/pages/story/components/SceneHeading.jsx
@@ -5,7 +5,6 @@ export default function SceneHeading({ scene }) {
 
   return (
     <header className="story-heading">
-      {scene.label ? <p className="story-label">{scene.label}</p> : null}
       {scene.title ? <h1 className="story-title">{scene.title}</h1> : null}
       {scene.headline ? <p className="story-headline">{scene.headline}</p> : null}
     </header>
